bdf73072a0cb5dc4bc96036291c5edf179985b2c
Kevin Penney <kpenney@gmail.com>
2020-09-03 20:44:15 -0700

    footer done

--- src/ui/Footer.jsx
+++ src/ui/Footer.jsx
@@ -3,18 +3,26 @@ import React from 'react'
 const Footer = () => {
    return (
       <div className='footer'>
-         Icons made by{' '}
-         <a href='https://smashicons.com/' title='Smashicons'>
-            Smashicons{' '}
-         </a>
-         and{' '}
-         <a href='https://www.flaticon.com/authors/prosymbols' title='Prosymbols'>
-            Prosymbols{' '}
-         </a>
-         from{' '}
-         <a href='https://www.flaticon.com/' title='Flaticon'>
-            www.flaticon.com
-         </a>
+         <p>
+            This is not a real store. It's just a fun project. Don't try too hard to buy anything!
+         </p>
+         <p>Copyright &copy; {new Date().getFullYear()} Kevin Penney. All Rights Reserved</p>
+         <p>Questions and suggestions are welcome! kevin@penneyprojects.com</p>
+         <br />
+         <p>
+            Icons made by{' '}
+            <a href='https://smashicons.com/' title='Smashicons'>
+               Smashicons{' '}
+            </a>
+            and{' '}
+            <a href='https://www.flaticon.com/authors/prosymbols' title='Prosymbols'>
+               Prosymbols{' '}
+            </a>
+            from{' '}
+            <a href='https://www.flaticon.com/' title='Flaticon'>
+               www.flaticon.com
+            </a>
+         </p>
       </div>
    )
 }
